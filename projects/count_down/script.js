d79600c1af494002b4c9f26a64f0346a3ed2b449
Syed Talha <syedt.in00@gmail.com>
2024-02-16 19:22:23 +0500

    modified

--- projects/count_down/script.js
+++ projects/count_down/script.js
@@ -3,8 +3,8 @@ function updateCountdown() {
     const now = new Date().getTime();
     const timeDifference = countDownDate - now;
   
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 25));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 25)) / (1000 * 60 * 60));
     const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
   
